50dbfcd8c990620f86c3acbdaa392c6d1c35c612
Nihad Kadić <106343121+KadicNihad@users.noreply.github.com>
2024-12-16 05:21:59 +0100

    delete surplas file

--- CMS/admin/js/redirectToEditPage.js
+++
@@ -1,6 +0,0 @@
-export function redirectToEditPage(id) {
-    localStorage.setItem("editArticleId", id);
-    window.location.href = "editArticle.html";
-}
-
-window.redirectToEditPage = redirectToEditPage;
