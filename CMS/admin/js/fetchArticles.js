0028ece75d0c301866f04d8637af0e6913653980
Nihad Kadić <106343121+KadicNihad@users.noreply.github.com>
2024-12-16 01:13:01 +0100

    window.onload added to the beginning, change name of function, deleted else, added response message

--- CMS/admin/js/fetchArticles.js
+++ CMS/admin/js/fetchArticles.js
@@ -1,44 +1,42 @@
-function fetchArticles () {
+window.onload = function fetchArticles() {
     const bearerToken = 'FsRRUFsYboQjJSHAdAouTQPWvHCCIOagwcbAOTFL';
     const apiUrl = 'https://api.webbytechexpert.com/devTest/api.php';
 
     fetch(apiUrl, {
-        method:"GET",
+        method: "GET",
         headers: {
-            "Authorization": `Bearer ${bearerToken}`
-        }
+            "Authorization": `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        },
     }).then(response => {
-        if(!response.ok) {
-            throw new Error(`HTTP error! Status:${response.status}`);
-        }
         return response.json();
     }).then(articles => {
         const tableBody = document.getElementById("articlesTable").querySelector("tbody");
         tableBody.innerHTML = "";
 
-        if (articles.length){
+        if (articles.length) {
             articles.forEach(article => {
                 const row = document.createElement("tr");
-                row.id= article.id;
-                row.innerHTML= `
+                row.id = article.id;
+                row.innerHTML = `
                 <td>${article.id}</td>
                 <td>${article.title}</td>
                 <td>${article.picture}</td>
                 <td>${article.createdAt}</td>
                 <td>
                      <button type="button" class="btn btn-warning" onclick="openEditPage(${article.id})">Edit</button>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" onclick="openDeleteModal(${article.id})" data-bs-target="#deleteArticleModal">Delete</button>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" onclick="OnOpenDeleteModalClick(${article.id})" data-bs-target="#deleteArticleModal">Delete</button>
                 </td>
                 `;
                 tableBody.appendChild(row);
             })
-        }else {
-                document.getElementById('noArticle').style.display='block';
-                document.getElementById('tableEmpty').style.display='none';
+            return ;
         }
+        document.getElementById('noArticle').style.display = 'block';
+        document.getElementById('tableEmpty').style.display = 'none';
+
     }).catch(error => {
-        console.error("Greska prilikom dobavljanja artikala:",error);
+        document.getElementById('message').innerText= error;
     })
 }
 
-window.onload = fetchArticles;
